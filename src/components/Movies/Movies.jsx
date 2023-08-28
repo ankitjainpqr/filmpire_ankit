ffd366056a43e82425167e5131b9c97f208ebb1f
Ankit Jain <ankitjain@Ankits-Mac-mini.local>
2023-08-28 08:43:42 +0530

    pushing code

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+import {MovieList} from '..';
+import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
 const Movies = () => {
+  const [page, setPage] = useState(1);
+  const {genreIdOrCategoryName} = useSelector((state) => state.currentGenreOrCateory || {});
+  // console.log('aa', genreIdOrCategoryName)
+  const {data, error, isFetching} = useGetMoviesQuery({ genreIdOrCategoryName, page});
+  useEffect(() => {
+    console.log('genreIdOrCategoryName',genreIdOrCategoryName);
+  })
+  
+  if(isFetching){
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    )
+  }
+  if(!data.results.length){
+    return(
+      <Box display="flex" alignItems="center" mt="20px" >
+        <Typography variant='h4'>
+        No movies match that name.
+        <br/>
+        Please search for something else.
+        </Typography>
+      </Box>
+    )
+  }
+  
+  if(error) return 'An error has occurred';
+
   return (
-    <div>Movies</div>
+    <div>
+      <MovieList movies={data} />
+    </div>
   )
 }
 
